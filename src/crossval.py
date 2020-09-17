f5324b97f78d80ae4423db247d7602517e995d8f
Ryan Tatton <rdt17@protonmail.com>
2020-09-17 18:03:49 -0400

    Cleans up cross_val (paired programming)
    - Uses divmod() instead of manually computing divisor and remainder.
    - Found a few bugs related to variable assignment.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- src/crossval.py
+++ src/crossval.py
@@ -1,36 +1,28 @@
 'https://medium.com/dev-genius/python-for-experienced-programmers-a2ee334ce62f'
-#let's use dictionaries for our data
-import math
+# let's use dictionaries for our data
 import random
+from typing import Any, Iterable
+
+
 def find_indices(list, condition):
-    return [i for i, elem in enumerate(list) if condition(elem)]
+	return [i for i, elem in enumerate(list) if condition(elem)]
 
 
-def crossval(labels, folds, iterations):
+def cross_val(labels: Iterable[Any], folds: int):
+	pos_lab = find_indices(labels, lambda x: x > 0)
+	neg_lab = find_indices(labels, lambda x: x <= 0)
+	pos_per_fold, pos_remainder = divmod(len(pos_lab), folds)
+	neg_per_fold, neg_remainder = divmod(len(neg_lab), folds)
 
-    pos_lab = find_indices(labels, lambda x: x>0)
-    neg_lab = find_indices(labels, lambda x: x<=0)
-    pos_per_fold = math.floor(len(pos_lab) / folds)
-    neg_per_fold = math.floor(len(neg_lab) / folds)
-    pos_remainder = math.modf(len(pos_lab) / folds)
-    neg_remainder = math.modf(len(neg_lab) / folds)
-
-    pos_lab_shuff = pos_lab[:]
-    neg_lab_shuff = neg_lab[:]
-    random.shuffle(pos_lab_shuff)
-    random.shuffle(neg_lab_shuff)
-    data = []
-    for x in range(1,folds):
-        pos_count =  pos_per_fold + min(1, pos_remainder)
-        neg_count =  neg_per_fold + min(1, neg_remainder)
-        data[x] = [pos_lab_shuff[1:pos_count] + neg_lab_shuff[1:neg_count]]
-        pos_per_fold = max(0, pos_per_fold - 1)
-        neg_per_fold = max(0, neg_per_fold - 1)
-        del pos_lab_shuff[1:pos_count]
-        del neg_lab_shuff[1:neg_count]
-    return data
-
-
-
-
-
+	random.shuffle(pos_lab)
+	random.shuffle(neg_lab)
+	data = []
+	for x in range(folds):
+		pos_count = pos_per_fold + min(1, pos_remainder)
+		neg_count = neg_per_fold + min(1, neg_remainder)
+		data[x] = [pos_lab[:pos_count] + neg_lab[:neg_count]]
+		pos_remainder = max(0, pos_remainder - 1)
+		neg_remainder = max(0, neg_remainder - 1)
+		del pos_lab[:pos_count]
+		del neg_lab[:neg_count]
+	return data
