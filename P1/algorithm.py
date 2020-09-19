7b31831f4b1491ca34ad27c3994ca1b7a3a53007
Daniel Shao <dxs765@case.edu>
2020-09-18 23:55:14 -0400

    base condition for id3 and node class for decision tree added

--- P1/algorithm.py
+++ P1/algorithm.py
@@ -2,14 +2,11 @@ from typing import Iterable
 
 from observation import ObservationSet
 from mldata import Feature
-
-
+from collections import deque
+import numpy
 # TODO Develop data structure for tree: linked list?
 import P1.metrics
-
-
-def id3(dataset: ObservationSet, target: Feature, features: Iterable[Feature]):
-	"""
+"""
 
 	Args:
 		dataset: Collection of training examples.
@@ -20,16 +17,49 @@ def id3(dataset: ObservationSet, target: Feature, features: Iterable[Feature]):
 	Returns:
 		A decision tree that classifies the given observations.
 	"""
+
+def id3(dataset: ObservationSet, target: Feature, features: Iterable[Feature]):
+
 	pure_node = checkPure(target.to_float)
 	for feat in features:
 			if feat.Type == Feature.Type.NOMINAL:
 				ig = P1.gain_ratio
-			elif feat.Type == Feature.Type.CONTINUOUS #consider every boolean test
+			elif feat.Type == Feature.Type.CONTINUOUS: #consider every boolean test
+					partition = get_best_partition(feat)
+
+	#root = Node(bestF)a
 
 
-	#root = Node(bestF)
+def checkPure(iterator):
+   return len(set(iterator)) <= 1
 
-	"""
+def get_best_partition(feat, labels): #find every time the class label changes
+	splits = deque()
+
+	labelsN = numpy.array(labels)
+	featN=numpy.array(feat)
+	sorted_indices = numpy.argsort(featN)
+	featN=numpy.sort(featN)
+	labelsN = labelsN[sorted_indices]
+	prev_type = labelsN[0]
+	splits.append(0)
+	for x in range(1, len(labelsN)):
+		if labelsN[x] != prev_type:
+			#splits.append(featN(x)) #add to split if the type changes so we don't check every single label
+			splits.append(x)
+		prev_type = labelsN[x]
+	bestSplit = 0
+	while(splits): #could make this a dp problem. or greedy maybe? while splits is not empty - it'd be faster to sort feature first and get labels to correspond.
+		finish = splits.pop()
+		pos_belowCount = sum(labelsN[1:finish])
+		neg_belowCount = finish - pos_belowCount
+		pos_aboveCount = sum(labelsN[finish:len(labelsN)])
+		neg_aboveCount = len(labelsN) - pos_aboveCount
+		return bestSplit
+def find_indices(list, condition):
+	return [i for i, elem in enumerate(list) if condition(elem)]
+
+"""
 	ID3 pseudo-code (from Machine Learning textbook)
 	------------------------------------------------
 	1. Create a root node for the tree
@@ -54,5 +84,3 @@ def id3(dataset: ObservationSet, target: Feature, features: Iterable[Feature]):
 				id3(features_fi, target, features - {F})
 	Return root
 	"""
-def checkPure(iterator):
-   return len(set(iterator)) <= 1
