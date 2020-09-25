83336c72300198668a062204fef0c249ec5c4322
Ryan Tatton <rdt17@protonmail.com>
2020-09-25 11:01:28 -0400

    Cleans up metrics; adds probability()
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/metrics.py
+++ P1/metrics.py
@@ -1,49 +1,49 @@
 from collections import Counter
-from typing import Any, Iterable, SupportsFloat, SupportsInt, Union
+from typing import Collection, Union
 
 import numpy as np
-from collections import deque
-from P1 import algorithm
-
-Number = Union[SupportsInt, SupportsFloat]
 
 
-def gain_ratio(feature: Iterable[Any], ig: Number) -> float:
-    h_x = entropy(feature)
-    return ig / h_x
+def gain_ratio(feature, label) -> float:
+	return information_gain(feature, label) / entropy(feature)
 
 
-def information_gain(x: Iterable[Number], label: Iterable[Any]) -> float:
-    h_y = entropy(label)
-    uniqueVals = set(x)
-    all_entropies = 0
-    for val in uniqueVals:
-        x_prob, h_y_x = conditional_entropy(x, val, label)
-        all_entropies = all_entropies + x_prob * h_y_x
-    return h_y - all_entropies
-
-# h(y|X=condition)
-# x is a feature vector, y is the labels, cond is the specific value of x
-def conditional_entropy(x: Iterable[Number], cond: Iterable[Number], labels) -> float:
-    indices = algorithm.find_indices(x, lambda val: val == cond)
-    count = 0
-    for idx in indices:
-        if labels(idx) == 1:
-            count = count + 1
-    probability = count / len(indices)
-    x_prob = len(indices) / len(labels)
-    return x_prob, -probability * (numpy_log(probability, 2)) - (1 - probability) * numpy_log(1 - probability, 2)
+def information_gain(feature, label) -> float:
+	return entropy(label) - conditional_entropy(label, feature)
 
 
-def entropy(x: Iterable[Number], base=2) -> float:
-    frequencies = np.array(Counter(x).values())
-    probabilities = frequencies / len(frequencies)
-    return - expectation(numpy_log(probabilities, base), probabilities)
+def conditional_entropy(event, given) -> float:
+	return sum(
+		expectation(g, entropy(conditional_probability(event, e, given, g)))
+		for e in set(event) for g in set(given)
+	)
 
 
-def expectation(x: Iterable[Number], probabilities: Iterable[Number]) -> float:
-    return sum(np.array(x) * probabilities)
+def conditional_probability(event, event_val, given, given_val) -> float:
+	unconditional = probability(given, given_val)
+	joint_freq = sum(
+		1 for e, g in zip(event, given) if e == event_val and g == given_val
+	)
+	joint = joint_freq / len(min(event, given))
+	return joint / unconditional
 
 
-def numpy_log(x: Iterable[Number], base: Number) -> Union[float, np.ndarray]:
-    return np.log(x) / np.log(base)
+def entropy(probability, base=2) -> float:
+	return - expectation(numpy_log(probability, base), probability)
+
+
+def probability(x: Collection, val=None) -> Union[float, Collection[float]]:
+	frequency = Counter(x)
+	if val is not None:
+		frequency = frequency[val]
+	else:
+		frequency = np.array(frequency.values())
+	return frequency / len(x)
+
+
+def expectation(x, probability) -> float:
+	return sum(np.array(x) * np.array(probability))
+
+
+def numpy_log(x, base) -> Union[float, Collection[float]]:
+	return np.log(x) / np.log(base)
