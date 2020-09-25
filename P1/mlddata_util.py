031f0625fa711f023aa4a3450e699eb8bea0a553
Ryan Tatton <rdt17@protonmail.com>
2020-09-25 13:34:19 -0400

    Adds get_feature_examples() and get_labels()
    Does not include the ID feature in the get_features tuple.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/mlddata_util.py
+++ P1/mlddata_util.py
@@ -1,10 +1,18 @@
 from typing import Tuple
 
-from .mldata import ExampleSet, Feature
+from mldata import ExampleSet, Feature
 
 
 def get_features(example_set: ExampleSet, example_index: int) -> Tuple:
-	return tuple(example_set[example_index][:-1])
+	return tuple(example_set[example_index][1:-1])
+
+
+def get_feature_examples(example_set: ExampleSet, feature_index: int) -> Tuple:
+	return tuple(example[feature_index] for example in example_set)
+
+
+def get_labels(example_set: ExampleSet):
+	return tuple(example[-1] for example in example_set)
 
 
 def get_label(example_set: ExampleSet, example_index: int):
