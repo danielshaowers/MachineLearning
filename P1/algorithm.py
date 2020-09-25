23165169c855c41d48fdac0ecffe5d7868f85db2
Ryan Tatton <rdt17@protonmail.com>
2020-09-25 15:09:05 -0400

    Finalizing ID3 still in-progress
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/algorithm.py
+++ P1/algorithm.py
@@ -1,13 +1,12 @@
 from abc import ABC, abstractmethod
-from collections import Counter, deque, namedtuple
+from collections import deque, namedtuple
 
 import numpy as np
 
-from .Node import Node
-# TODO Fix this -- not good coding practice
-from .metrics import gain_ratio, information_gain
-from .mldata import Feature
-from .observation import Observation, ObservationSet
+import metrics
+import mlddata_util as util
+from Node import Node
+from mldata import ExampleSet, Feature
 
 
 # for continuous and nominal, we don't need to remove them after we come up
@@ -16,27 +15,17 @@ from .observation import Observation, ObservationSet
 # ask for clarification what these each are. what is features vs dataset? is
 # dataset just a compilation of label and features?
 
-def get_labels(dataset: ObservationSet):
-	return [o.label for o in dataset]
-
-
-def is_pure(dataset: ObservationSet) -> bool:
-	return len(set(get_labels(dataset))) == 1
-
-
-def get_majority_label(dataset: ObservationSet):
-	label_counter = Counter([o.label for o in dataset])
-	return label_counter.most_common()
-
-
 class Model(ABC):
 
+	def __init__(self):
+		super(Model, self).__init__()
+
 	@abstractmethod
-	def train(self, data: ObservationSet):
+	def train(self, data: ExampleSet):
 		pass
 
 	@abstractmethod
-	def predict(self, observation: Observation):
+	def predict(self, data: ExampleSet):
 		pass
 
 
@@ -48,74 +37,73 @@ class ID3(Model):
 		else:
 			self.grow_full_tree = False
 		self.model_metrics = dict()
+		super(ID3, self).__init__()
 
-	def train(self, dataset: ObservationSet):
-		return self.id3(dataset, Node())
+	def train(self, data: ExampleSet) -> Node:
+		return self.id3(data, Node())
 
-	def predict(self, observation: Observation):
+	def predict(self, data: ExampleSet):
 		pass
 
-	# TODO How do we keep track of the features we have tested? Per step 3 of
-	#  the algorithm pseudo code, if we have tested all of our features,
-	#  then we should just return the majority class. In our case,
-	#  if we implicitly know what has been tested (vacuously true if the
-	#  feature is not in the partitioned dataset), then we'll only get the
-	#  case where we get a pure node?
-	def id3(self, dataset: ObservationSet, parent: Node, depth: int = 0):
-		"""
-		Args:
-			dataset: Collection of training examples.
-			parent:
+	def id3(self, dataset: ExampleSet, parent: Node, depth: int = 0):
+		"""Generates a decision tree using the ID3 algorithm.
 
-		Returns:
-			A decision tree that classifies the given observations.
-
-		ID3 pseudo-code (from Machine Learning textbook):
-			1. Create a root node for the tree
-			2. If all observations in the dataset are of the same class, return
-				the single-node tree with that class label
+		Pseudo-code (from Machine Learning, Mitchell):
+			1. Create a root node for the tree.
+			2. If all examples in the dataset are of the same class, return
+				the single-node tree with that class label.
 			3. If features is empty, return the single-node tree with the
-				target label that is most common among the observations
+				target label that is most common among the observations.
 			4. Otherwise:
 				4.1 Let F be the feature in features that "best" classifies
-					observations
-				4.2 Let root be F
+					observations.
+				4.2 Let root be F.
 				4.3 For each possible value f_i of F:
 					4.3.1 Add a new tree branch below root, corresponding to
-						the test F = f_i (this needs to be generalized to
-						account for continuous features as well)
+						the test F = f_i.
 					4.3.2 Let features_fi be the subset of features that have
-						value f_i for F
+						value f_i for F.
 					4.3.3 If features_fi is empty, then below this new branch,
 						add a leaf node with the most common value of target
-						among the observations as the label
+						among the observations as the label.
 					4.3.4 Otherwise, below this new branch, add the subtree
-						id3(features_fi, target, features - {F})
-			Return root
+						id3(features_fi, target, features - {F}).
+			Return root.
+
+		Args:
+			dataset: Collection of training examples.
+			parent: Parent node in the decision tree.
+			depth: Current depth of the decision tree.
+
+		Returns:
+			Decision tree that classifies the given observations.
 		"""
 
 		# TODO Other stopping criteria: at max_depth, no more features/tests
-		if is_pure(dataset):
-			return Node(data=get_labels(dataset)[0], parent=parent)
+		if util.is_homogeneous(dataset):
+			return Node(data=util.get_labels(dataset)[0], parent=parent)
 
-		# 4.1 Get best feature and make it the root of the subtree
-		def get_best_feature(dataset: ObservationSet):
-			features = dataset.to_features()
-			for feat in features:
-
-				if feat.Type == Feature.Type.NOMINAL:
-					if igflag:
-						ig = information_gain()
-					else:
-						ig = gain_ratio()
+	# 4.1 Get best feature and make it the root of the subtree
+	@staticmethod
+	def _get_best_feature(data: ExampleSet, use_info_gain: bool = True):
+		labels = util.get_labels(data)
+		features_examples = util.get_feature_examples(data)
+		types = [feature.type for feature in util.get_features_info(data)]
+		for f_examples, f_type in zip(features_examples, types):
+			if f_type == Feature.Type.NOMINAL:
+				if use_info_gain:
+					split_criteria_val = metrics.info_gain(labels, f_examples)
 				else:
-					partition = get_best_partition(feat, target, igflag)
+					split_criteria_val = metrics.gain_ratio(labels, f_examples)
+			else:
+				partition = \
+					ID3._get_best_partition(f_examples, labels, use_info_gain)
 
-				feat_array = np.array(best_feat)
-				lChild_idx = np.argwhere(feat_array <= partition)
-				rChild_idx = numpy.argwhere(feat_array > partition)
-				leftChild = parent_node.left(Node(parent_node, partition))
-				rightChild = parent_node.right(Node(parent_node, partition))
+			feat_array = np.array(best_feat)
+			lChild_idx = np.argwhere(feat_array <= partition)
+			rChild_idx = numpy.argwhere(feat_array > partition)
+			leftChild = parent_node.left(Node(parent_node, partition))
+			rightChild = parent_node.right(Node(parent_node, partition))
 
 	# 4.2 Make the best feature the root of the subtree
 
@@ -133,18 +121,20 @@ class ID3(Model):
 	# root = Node(bestF)a
 
 	# find every time the class label changes
-	def get_best_partition(self, feature: Feature, label: Feature,
-						   use_ig: bool):
-		ValueLabelPair = namedtuple('ValueLabelPair', ['value', 'label'])
-		feature_label_pairs = [
-			ValueLabelPair(v, l) for v, l in zip(feature.values, label.values)
-		]
-		feature_label_pairs.sort(key=lambda p: p.value)
+	@staticmethod
+	def _get_best_partition(feature_examples, labels, use_info_gain: bool):
+		ValueLabel = namedtuple('ValueLabel', ['value', 'label'])
+		pairs = [ValueLabel(v, l) for v, l in zip(feature_examples, labels)]
+		pairs = tuple(sorted(pairs, key=lambda p: p.value))
 		splits = []
-		prev = feature_label_pairs[0]
-		for pair in feature_label_pairs:
+		prev = pairs[0]
+		for i_pair in range(1, len(pairs)):
+			pair = pairs[i_pair]
 			if pair.label != prev.label:
 				splits.append(pair.value)
+			prev = pair
+
+		info_gain = metrics.info_gain(labels, feature_examples)
 
 		splits = deque()
 		labelsN = np.array(labels)
@@ -163,14 +153,12 @@ class ID3(Model):
 		bestSplit = 0
 		scoring = np.array(splits)
 		cnt = 0;
-		while splits:  # could make this a dp problem. or greedy maybe? while
-			# splits is not empty - it'd be faster to sort
-			# feature first and get labels to correspond.
+		while splits:
 			finish = splits.pop()
 			split_feats = np.zeros(finish) + np.zeros(len(labelsN) - finish)
 			if use_ig:
 				# feed in the labels and
-				scoring[cnt] = information_gain(split_feats, labelsN)  #
+				scoring[cnt] = info_gain(split_feats, labelsN)  #
 			else:
 				scoring[cnt] = gain_ratio(split_feats, labelsN)
 			cnt = cnt + 1
