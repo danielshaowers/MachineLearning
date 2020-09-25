d6aef794c8518438085137afaeb38089b956de8f
Ryan Tatton <rdt17@protonmail.com>
2020-09-25 13:48:51 -0400

    Adds the ability to get multiple features, labels, and feature infos
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/mlddata_util.py
+++ P1/mlddata_util.py
@@ -1,23 +1,31 @@
 from typing import Tuple
 
-from mldata import ExampleSet, Feature
+from mldata import ExampleSet
 
 
-def get_features(example_set: ExampleSet, example_index: int) -> Tuple:
-	return tuple(example_set[example_index][1:-1])
+def get_features(example_set: ExampleSet, example_index: int = None) -> Tuple:
+	if example_index is None:
+		features = tuple(tuple(example[1:-1] for example in example_set))
+	else:
+		features = tuple(example_set[example_index][1:-1])
+	return features
 
 
-def get_feature_examples(example_set: ExampleSet, feature_index: int) -> Tuple:
-	return tuple(example[feature_index] for example in example_set)
+def get_feature_examples(example_set: ExampleSet, index: int) -> Tuple:
+	return tuple(example[index] for example in example_set)
 
 
-def get_labels(example_set: ExampleSet):
-	return tuple(example[-1] for example in example_set)
+def get_labels(example_set: ExampleSet, index: int = None):
+	if index is None:
+		labels = tuple(example[-1] for example in example_set)
+	else:
+		labels = example_set[index][-1]
+	return labels
 
 
-def get_label(example_set: ExampleSet, example_index: int):
-	return example_set[example_index][-1]
-
-
-def get_feature_info(example_set: ExampleSet, feature_index: int) -> Feature:
-	return example_set.schema[feature_index]
+def get_features_info(example_set: ExampleSet, index: int = None):
+	if index is None:
+		info = tuple(example_set.schema[1:])
+	else:
+		info = example_set.schema[index]
+	return info
