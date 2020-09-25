7e5294f9ce6c74dc8837f0edd3f16f613df8bc49
Ryan Tatton <rdt17@protonmail.com>
2020-09-25 14:05:36 -0400

    Adds helper methods from algorithm.py to mlddata_util.py
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/mlddata_util.py
+++ P1/mlddata_util.py
@@ -1,31 +1,40 @@
+from collections import Counter
 from typing import Tuple
 
 from mldata import ExampleSet
 
 
-def get_features(example_set: ExampleSet, example_index: int = None) -> Tuple:
+def is_homogeneous(data: ExampleSet) -> bool:
+	return len(set(get_labels(data))) == 1
+
+
+def get_majority_label(data: ExampleSet):
+	return Counter(get_labels(data)).most_common()
+
+
+def get_features(data: ExampleSet, example_index: int = None) -> Tuple:
 	if example_index is None:
-		features = tuple(tuple(example[1:-1] for example in example_set))
+		features = tuple(tuple(example[1:-1] for example in data))
 	else:
-		features = tuple(example_set[example_index][1:-1])
+		features = tuple(data[example_index][1:-1])
 	return features
 
 
-def get_feature_examples(example_set: ExampleSet, index: int) -> Tuple:
-	return tuple(example[index] for example in example_set)
+def get_feature_examples(data: ExampleSet, index: int) -> Tuple:
+	return tuple(example[index] for example in data)
 
 
-def get_labels(example_set: ExampleSet, index: int = None):
+def get_labels(data: ExampleSet, index: int = None):
 	if index is None:
-		labels = tuple(example[-1] for example in example_set)
+		labels = tuple(example[-1] for example in data)
 	else:
-		labels = example_set[index][-1]
+		labels = data[index][-1]
 	return labels
 
 
-def get_features_info(example_set: ExampleSet, index: int = None):
+def get_features_info(data: ExampleSet, index: int = None):
 	if index is None:
-		info = tuple(example_set.schema[1:])
+		info = tuple(data.schema[1:])
 	else:
-		info = example_set.schema[index]
+		info = data.schema[index]
 	return info
