b59f87f8afcf24c6cebb720aaed3959a3b28c628
Ryan Tatton <rdt17@protonmail.com>
2020-09-29 02:01:15 -0400

    Adds is_leaf(), has_left_child(), has_right_child(), get_max_depth(), and get_tree_size()
    - The first three functions are simple helper methods that are used in algorithm.py. The decision tree built by ID3 has two types of nodes. Being able to check if the node is leaf helps determine what type of data the node contains.
    - The last two functions are used to get metrics about the tree. They are both recursive due to the nature of how the tree is structured.
    
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/Node.py
+++ P1/Node.py
@@ -4,3 +4,26 @@ class Node:
 		self.right = right
 		self.parent = parent
 		self.data = data
+
+	def is_leaf(self):
+		return not self.has_left_child() and not self.has_right_child()
+
+	def has_left_child(self):
+		return self.left is not None
+
+	def has_right_child(self):
+		return self.right is not None
+
+	def get_max_depth(self, depth: int = 0) -> int:
+		if self.has_left_child():
+			depth = max(self.left.get_max_depth(depth + 1), depth)
+		if self.has_right_child():
+			depth = max(self.right.get_max_depth(depth + 1), depth)
+		return depth
+
+	def get_tree_size(self, tree_size: int = 1) -> int:
+		if self.has_left_child():
+			tree_size += self.left.get_tree_size(tree_size + 1)
+		if self.has_right_child():
+			tree_size += self.right.get_tree_size(tree_size + 1)
+		return tree_size
