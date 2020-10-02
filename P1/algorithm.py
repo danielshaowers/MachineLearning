28778186b45e5b3fbb75c463daa70ead6c28958a
Ryan Tatton <rdt17@protonmail.com>
2020-10-02 11:51:02 -0400

    Minor clean-up; updates to account for the as_tuple flag for mlutil functions
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/algorithm.py
+++ P1/algorithm.py
@@ -1,5 +1,4 @@
 import enum
-import itertools
 from abc import ABC, abstractmethod
 from typing import Callable, NoReturn, Tuple
 
@@ -74,16 +73,16 @@ class ID3(Model):
 		return predictions
 
 	# recursively predicts the label of a single example
-	def _predict_example(self, example: mldata.ExampleSet, root: node.Node):
-		if root.is_leaf():  # base condition. returns true or false
-			return root.data
-		index = mlutil.get_feature_index(example, root.data.feature)
-		feature_value = mlutil.get_features(example, index)
-		result: bool = root.data.evaluate(feature_value)
+	def _predict_example(self, example: mldata.ExampleSet, model: node.Node):
+		if model.is_leaf():  # base condition. returns true or false
+			return model.data
+		idx = mlutil.get_feature_index(example, model.data.feature)
+		val = mlutil.get_features(example, example_index=0, feature_index=idx)
+		result: bool = model.data.evaluate(val)
 		if result:
-			predicted = self._predict_example(example, root.left)
+			predicted = self._predict_example(example, model.left)
 		else:
-			predicted = self._predict_example(example, root.right)
+			predicted = self._predict_example(example, model.right)
 		return predicted
 
 	# main id3 method to build a decision tree recursively
@@ -103,72 +102,58 @@ class ID3(Model):
 				Decision tree that classifies the given observations.
 			"""
 		majority_label = mlutil.get_majority_label(data)
-		# check base conditions, if we're at max depth or pure
 		if mlutil.is_homogeneous(data) or self._at_max_depth(depth):
-			# return a leaf node with the
 			return node.Node(data=majority_label, parent=parent)
-		# majority label
-		# helper method  find the best feature of all available
-		feature_idx, test = self._get_best_feature_and_test(data)
-		feature = mlutil.get_feature_examples(data, feature_idx)
-		# helper class to run our tests
+		feature, test = self._get_best_feature_and_test(data)
 		parent.data = Test(feature=feature, test=test)
-		# separate data
-		left_data, right_data = self._partition_data(data, feature_idx, test)
-		# into two groups according to our best test
-		# if there are no elements in one partition,
+		left_data, right_data = self._partition_data(data, feature, test)
 		if len(left_data) == 0:
-			# return a leaf of our current majority label
 			left_child = node.Node(data=majority_label, parent=parent)
-		# set to current majority label
-		else:  # set left child to be results of one test
+		else:
 			left_child = self.id3(left_data, parent, depth + 1)
 		parent.left = left_child
 		if len(right_data) == 0:
 			right_child = node.Node(data=majority_label, parent=parent)
 		else:
 			right_child = self.id3(right_data, parent, depth + 1)
-		parent.right = right_child  # update our node's children
+		parent.right = right_child
 		return parent
 
-	# checks all possible tests for every feature and returns the best test
 	def _get_best_feature_and_test(self, data: mldata.ExampleSet) -> Tuple:
 		labels = mlutil.get_labels(data)
-		features = mlutil.get_feature_examples(data, 2)
+		feature_exs = mlutil.get_feature_examples(data, as_tuple=False)
 		split_tests = mlutil.create_all_split_tests(data)
-		label_tests = mlutil.create_split_tests(
-			labels, mldata.Feature.Type.BINARY)
-		split_values = [
-			# finds the information gain or gain ratio of each test
-			[
-				self.split_function(labels, label_tests, f, [t]) for t in
-				split_tests[i]]
-			for i, f in enumerate(features)]
+		label_type = mlutil.get_label_info(data).type
+		label_tests = mlutil.create_split_tests(labels, label_type)
+		# todo: split_values is getting the same values every time :(
+		# finds the information gain or gain ratio of each test
+		# self.split_function(labels, label_test, feature_vals,
+		# feature_tests
+		split_values = [[
+			self.split_function(labels, label_tests, f, [t])
+			# get the tests for the ith feature
+			for t in split_tests[i]] for i, f in enumerate(feature_exs)]
 		i_max_feature = int(np.argmax([max(v) for v in split_values]))
 		i_max_test = np.argmax(split_values[i_max_feature])
 		best_test = split_tests[i_max_feature][i_max_test]
 		best_feature = data.schema[i_max_feature]
-		#best_feature = i_max_feature  # + 2 may want to add two to correspond
+		# best_feature = i_max_feature  # + 2 may want to add two to correspond
 		# to the ones we ignored
-		return i_max_feature, best_test
-	#todo: confirm the indexing with i_max feature is correct
-	#separate data according to our test
+		return best_feature, best_test
+
+	# todo: confirm the indexing with i_max feature is correct
 	@staticmethod
 	def _partition_data(
 			data: mldata.ExampleSet,
-			idx,
+			feature: mldata.Feature,
 			test: Callable) -> Tuple:
-		#idx = mlutil.get_feature_index(data, feature)
+		idx = mlutil.get_feature_index(data, feature)
 		left_data = mldata.ExampleSet([e for e in data if test(e[idx])])
 		right_data = mldata.ExampleSet([e for e in data if not test(e[idx])])
 		return left_data, right_data
 
 	def _at_max_depth(self, depth: int) -> bool:
-		if self.max_depth < 1:
-			at_max = False
-		else:
-			at_max = depth == self.max_depth
-		return at_max
+		return False if self.max_depth < 1 else depth == self.max_depth
 
 
 class Test:
