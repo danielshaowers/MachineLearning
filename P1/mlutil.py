0acd56de03a523185d6eba139809e40f39611c3d
Ryan Tatton <rdt17@protonmail.com>
2020-10-02 01:34:36 -0400

    Adds find_split_values(), create_continuous_split_tests(), and create_discrete_split_tests()
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/mlutil.py
+++ P1/mlutil.py
@@ -2,48 +2,52 @@ import functools
 import operator
 from collections import Counter
 from numbers import Number
-from typing import Any, Callable, Collection, Iterable, Sequence, Tuple
+from typing import Any, Callable, Iterable, Sequence, Set, Tuple
 
 import mldata
-
-
-# TODO Account for continuous variables
-def create_all_split_tests(
-		data: mldata.ExampleSet,
-		drop_single_tests: bool = True) -> Tuple:
-	types = [f.type for f in get_features_info(data)]
-	examples = get_feature_examples(data)
-	tests = tuple(create_split_tests(e, t) for e, t in zip(examples, types))
-	if drop_single_tests:
-		tests = tuple(t for t in tests if len(t) > 1)
-	return tests
 
 
 def binarize_feature(values: Iterable, test: Callable[[Any], bool]) -> Tuple:
 	return tuple(test(v) for v in values)
 
 
-def find_partition_values(values: Sequence[Number], labels: Sequence):
-	sorted_pairs = sorted(zip(values, labels), key=lambda x: x[0])
-	return set(
-		sum((sorted_pairs[i][0], sorted_pairs[i - 1][0])) / 2
-		for i in range(1, min(len(values), len(labels)))
-		if sorted_pairs[i - 1][1] != sorted_pairs[i][1]
-	)
+def create_all_split_tests(
+		data: mldata.ExampleSet,
+		drop_single_tests: bool = True) -> Tuple:
+	types = (feature.type for feature in get_features_info(data))
+	exs = get_feature_examples(data)
+	labels = get_labels(data)
+	tests = (create_split_tests(e, t, labels) for e, t in zip(exs, types))
+	if drop_single_tests:
+		tests = (t for t in tests if len(t) > 1)
+	return tuple(tests)
 
 
 def create_split_tests(
-		values: Iterable,
-		feature_type: mldata.Feature.Type) -> Tuple:
-	unique = set(values)
-	if feature_type in {
-		mldata.Feature.Type.NOMINAL, mldata.Feature.Type.BINARY}:
-		tests = tuple(functools.partial(operator.eq, v) for v in unique)
-	elif feature_type == mldata.Feature.Type.CONTINUOUS:
-		tests = tuple(functools.partial(operator.le, v) for v in unique)
-	else:
-		tests = tuple()
+		values: Sequence,
+		feature_type: mldata.Feature.Type,
+		labels: Sequence = None) -> Tuple[Callable]:
+	tests = tuple()
+	categorical = {mldata.Feature.Type.NOMINAL, mldata.Feature.Type.BINARY}
+	if feature_type in categorical:
+		tests = create_discrete_split_tests(values)
+	elif feature_type == mldata.Feature.Type.CONTINUOUS and labels is not None:
+		split_values = find_split_values(values, labels)
+		tests = create_continuous_split_tests(split_values)
 	return tests
+
+
+def create_discrete_split_tests(values: Iterable) -> Tuple:
+	return tuple(functools.partial(operator.eq, v) for v in set(values))
+
+
+def find_split_values(values: Sequence[Number], labels: Sequence) -> Set:
+	ex = sorted(zip(values, labels), key=lambda x: x[0])
+	return {ex[i][0] for i in range(1, len(ex)) if ex[i - 1][1] != ex[i][1]}
+
+
+def create_continuous_split_tests(split_values: Iterable[Number]) -> Tuple:
+	return tuple(functools.partial(operator.le, v) for v in set(split_values))
 
 
 def is_homogeneous(data: mldata.ExampleSet) -> bool:
@@ -92,5 +96,5 @@ def get_feature_index(data: mldata.ExampleSet, feature: mldata.Feature) -> int:
 	return data.schema.index(feature)
 
 
-def get_label_info(data: mldata.ExampleSet) -> Collection:
+def get_label_info(data: mldata.ExampleSet) -> mldata.Feature:
 	return data.schema[-1]
