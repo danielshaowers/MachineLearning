073f795c1eefa5d34018097c1425f5ddda06b832
Ryan Tatton <rdt17@protonmail.com>
2020-10-02 19:28:50 -0400

    Added docstrings to crossval.py
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P1/crossval.py
+++ P1/crossval.py
@@ -2,20 +2,42 @@ import functools
 import operator
 import random
 from collections import defaultdict
-from typing import List, Sequence, Tuple
+from typing import Sequence, Tuple
 
 import mldata
 import mlutil
 
 
-def get_train_test_split(folds: Sequence[Sequence], test_fold_ind: int):
+def get_train_test_split(folds: Sequence, test_fold_ind: int) -> Tuple:
+	"""Creates the training and test sets from dataset folds.
+	Args:
+		folds: A sequence of sequences, each being a fold of the data.
+		test_fold_ind: Index of the training set fold.
+
+	Returns:
+		A training set and test set.
+	"""
 	train_folds = [folds[i] for i in range(len(folds)) if i != test_fold_ind]
 	train_set = mldata.ExampleSet(functools.reduce(operator.add, train_folds))
 	test_set = mldata.ExampleSet(folds[test_fold_ind])
 	return train_set, test_set
 
 
-def get_folds(dataset: mldata.ExampleSet, n_folds: int) -> Tuple[List]:
+def get_folds(dataset: mldata.ExampleSet, n_folds: int) -> Tuple:
+	"""Creates n stratified folds from the dataset.
+
+	For each unique value of the label, examples from the dataset are
+	assigned to a fold round robin style. It may be the case that some folds
+	have more training examples than others, depending  on the number of
+	folds and number of examples in the dataset.
+
+	Args:
+		dataset: Collection of training examples on which to generate folds.
+		n_folds: Number of folds to create.
+
+	Returns:
+		Tuple of lists, where each list is a fold of examples.
+	"""
 	folds = defaultdict(list)
 	labels = mlutil.get_labels(dataset)
 	for label in set(labels):
