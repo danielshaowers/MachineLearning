48e40cfe919fc28b4c5b262a93eab47a44945907
Ryan Tatton <rdt17@protonmail.com>
2020-10-09 18:53:57 -0400

    Some clean up of logreg.py, but getting stack overflow error
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- P2/logreg.py
+++ P2/logreg.py
@@ -12,51 +12,55 @@ import model
 class LogisticRegression(model.Model):
 
 	def __init__(self, cost: float = 1):
-		# Add any additional model parameters
 		super(LogisticRegression, self).__init__()
 		self.weights = None
 		self.cost = cost
 
-	def preprocess(self, data: mldata.ExampleSet):
-		ndata = mlutil.convert_to_numpy(data)
-		return mlutil.quantify_nominals(ndata[0], ndata[1])
+	@staticmethod
+	def preprocess(data: mldata.ExampleSet):
+		np_data, f_types = mlutil.convert_to_numpy(data)
+		return mlutil.quantify_nominals(np_data, f_types)
 
-	def train(self, data: mldata.ExampleSet, truths: np.array):
-		npdata = self.preprocess(data)
-		weights = np.random.rand(
-			len(npdata))  # randomly initialize weights for each feature
-		bias = np.random.rand(len(npdata),
-							  1)  # randomly initialize biases? not sure if i
-		# should even incorporate biases
-		self.weights = self.gradient_descent(npdata, truths, weights,
-											 stepsize=1, skip=set())
-		# okay now the weights should be finalized
-		return self.weights
+	def train(self, data: mldata.ExampleSet):
+		truths = mlutil.get_labels(data)
+		np_data = self.preprocess(data)
+		# randomly initialize weights for each feature
+		weights = np.random.rand(len(np_data))
+		# randomly initialize biases? should i even incorporate biases
+		bias = np.random.rand(len(np_data), 1)
+		self.weights = self.gradient_descent(
+			np_data, truths, weights, stepsize=1, skip=set()
+		)
 
-	def sigmoid(self, x, bias=0):
+	# okay now the weights should be finalized
+
+	@staticmethod
+	def sigmoid(x, bias=0):
 		return 1 / (1 + np.exp(-x + bias))
 
-	def weighted_input(self, x, weights):
+	@staticmethod
+	def weighted_input(x, weights):
 		return np.dot(x, weights)
 
 	def prob(self, weight, x):
 		return self.sigmoid(self.weighted_input(weight, x))
 
-	def conditional(self, x, y):
-		return -y * np.math.log(x) - (1 - y) * np.math.log(
-			1 - x)  # calculate log likelihood to maintain convexity instead
-
-	# of squared loss
+	@staticmethod
+	def conditional(x, y):
+		# calculate log likelihood to maintain convexity instead of squared
+		# loss
+		return -y * np.math.log(x) - (1 - y) * np.math.log(1 - x)
 
 	def predict(self, data: mldata.ExampleSet):
 		# guesses = np.zeros(len(ndata[1]), 1) # use sigmoid to find guesses
 		# guesses[np.where(sigmoid >= 0.5)] = 1 # truth guess when >= 0.5
-		ndata = self.preprocess(data)
+		np_data = self.preprocess(data)
 		weighted_feats = np.transpose(
-			np.array([self.weights[i] * f for i, f in enumerate(ndata)]))
-		log_likelihood_scores = np.array([self.sigmoid(sum(w)) for w in
-										  weighted_feats])  # vector to save
-		# the sigmoid values for each example
+			np.array([self.weights[i] * f for i, f in enumerate(np_data)]))
+		# vector to save the sigmoid values for each example
+		log_likelihood_scores = np.array([
+			self.sigmoid(sum(w)) for w in weighted_feats
+		])
 		return log_likelihood_scores >= 0.5, log_likelihood_scores
 
 	# return [model.Prediction(value=sc > 0.5, confidence=sc) for i,
@@ -67,10 +71,11 @@ class LogisticRegression(model.Model):
 		neglabel_idx = np.argwhere(labels <= 0)
 		pos_sigmoids = sigmoids[poslabel_idx]
 		neg_sigmoids = sigmoids[neglabel_idx]
-		conditional_ll = (1 / 2) * np.square(
-			np.linalg.norm(weights)) + self.cost * (
-								 sum(-np.log(pos_sigmoids)) + sum(
-							 -np.log(1 - neg_sigmoids)))
+		squared_norm = np.square(np.linalg.norm(weights))
+		pos_log_sigmoids = sum(-np.log(pos_sigmoids))
+		neg_log_sigmoids = sum(-np.log(1 - neg_sigmoids))
+		neg_log_cond_like = pos_log_sigmoids + neg_log_sigmoids
+		conditional_ll = 0.5 * squared_norm + self.cost * neg_log_cond_like
 		return conditional_ll
 
 	# identify the log loss and update parameters accordingly
@@ -88,9 +93,9 @@ class LogisticRegression(model.Model):
 			weighted_feats = []
 			#	for i, f in enumerate(ndata):
 			#		weighted_feats.append(weights[i] * f)
-			weighted_feats = np.transpose(np.array([weights[i] * f for i, f in
-													enumerate(
-														ndata)]))  #
+			weighted_feats = np.transpose(
+				np.array([weights[i] * f for i, f in enumerate(ndata)])
+			)  #
 			# theta^T*x: multiply all examples for each feature value by its
 			# corresponding weight
 			# gradient calculated as dJ(theta)/d(theta_j) = 1/m (sum from 1 to
@@ -100,15 +105,15 @@ class LogisticRegression(model.Model):
 			# feature. so we loop over all features and store results as a
 			# numpy array
 			gradient = np.zeros(len(ndata))
-			for j in range(len(ndata)):
-				if j not in skip:
-					gradient[j] = (1 / len(ndata)) * np.sum(
-						gradient_func(sum(weighted_feats[:][i]), ndata[j][i],
-									  truths[i])
-						for i, f in enumerate(ndata[j]))  # i indexes examples
-
-			finished = np.argwhere(abs(
-				gradient) < epsilon)  # todo: check which is local minimum.
+			for j in (jj for jj in range(len(ndata)) if jj not in skip):
+				summation = sum(
+					gradient_func(
+						sum(weighted_feats[:][i]), ndata[j][i], truths[i])
+					for i, f in enumerate(ndata[j])  # i indexes examples
+				)
+				gradient[j] = (1 / len(ndata)) * summation
+			# todo: check which is local minimum.
+			finished = np.argwhere(abs(gradient) < epsilon)
 			# when loss stops decreasing? or is it when the derivative changes
 			# sign? important distinction
 			if len(finished) > 0:
